80213ce0cae4ba292b85402a2c90125e7e36fb37
Roman Dobosz <gryf73@gmail.com>
2023-01-03 16:27:00 +0100

    Tweaks here and there for cleaning up OS resources
    Change-Id: Iecb7c929575a298bb993129624a94e4aa1b41593

--- contrib/migration-cleanup.sh
+++ contrib/migration-cleanup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 clusterID=$(oc get infrastructure.config.openshift.io cluster -o=jsonpath='{.status.infrastructureName}')
 clusterTag="openshiftClusterID=${clusterID}"
@@ -20,15 +20,12 @@ echo "Removing Kuryr finalizers from all Services"
 removeNamespacedFinalizers services kuryr.openstack.org/service-finalizer
 
 echo "Removing all tagged loadbalancers from Octavia"
-for lb in `openstack loadbalancer list --tags $clusterTag -f value -c id`; do
+for lb in $(openstack loadbalancer list --tags $clusterTag -f value -c id); do
     openstack loadbalancer delete --cascade $lb
 done
 
 echo "Removing Kuryr finalizers from all KuryrLoadBalancer CRs. This will trigger their deletion"
 removeNamespacedFinalizers kuryrloadbalancers.openstack.org kuryr.openstack.org/kuryrloadbalancer-finalizers
-
-echo "Removing Kuryr service network"
-openstack network delete kuryr-service-network-${clusterID}
 
 echo "Removing Kuryr finalizers from all pods"
 removeNamespacedFinalizers pods kuryr.openstack.org/pod-finalizer
@@ -36,48 +33,42 @@ removeNamespacedFinalizers pods kuryr.openstack.org/pod-finalizer
 echo "Removing Kuryr finalizers from all KuryrPort CRs. This will trigger their deletion"
 removeNamespacedFinalizers kuryrports.openstack.org kuryr.openstack.org/kuryrport-finalizer
 
-echo "Removing Kuryr finalizers from all Network Policies"
-removeNamespacedFinalizers networkpolicy kuryr.openstack.org/networkpolicy-finalizer
-
-echo "Removing Kuryr network policies"
-for sgid in $(openstack security group list -f value -c ID -c Description | grep 'Kuryr-Kubernetes Network Policy' | cut -f 1 -d ' '); do
-    openstack security group rule delete $(openstack security group rule list $sgid -f value -c ID | xargs)
-    openstack security group delete $sgid
-done
-removeNamespacedFinalizers kuryrnetworkpolicies.openstack.org kuryr.openstack.org/networkpolicy-finalizer
-
 echo "Remove subports created by Kuryr from trunks"
-# TODO(dulek): Filtering trunks by tags - not supported by openstackclient, got to do it manually
-trunks=`openstack network trunk list -f value -c ID`
+trunks=$(python -c "import openstack; n = openstack.connect().network; print(\" \".join([x.id for x in n.trunks(any_tags=\"$clusterTag\")]))")
 i=0
 len=`wc -l <<< $trunks`
 for trunk in $trunks; do
     i=$((i+1))
-    echo "    Processing trunk $trunk, ${i}/${len}"
+    echo "    Processing trunk $trunk, ${i}/${len}, it may take a time due to every port examinig for '$clusterTag' containing."
     subports=()
-    for subport in `openstack network subport list --trunk $trunk -f value -c Port`; do
+    for subport in $(python -c "import openstack; n = openstack.connect().network; print(\" \".join([x['port_id'] for x in n.get_trunk(\"$trunk\").sub_ports if \"$clusterTag\" in n.get_port(x['port_id']).tags]))"); do
         subports+=($subport)
     done
-    # TODO(dulek): Check subports tags somehow, we don't want to remove user's subports.
-    if [ ${#subports[@]} -gt 0 ]; then
-        # FIXME(dulek): Something's wrong here, it expands into a single argument?
-        echo openstack network trunk unset ${subports[@]/#/--subport } $trunk
+
+    args=()
+    for sub in "${subports[@]}" ; do
+        args+=("--subport $sub")
+    done
+
+    if [ ${#args[@]} -gt 0 ]; then
+        openstack network trunk unset ${args[*]} $trunk
     fi
 done
 
 echo "Get all networks and subnets from KuryrNetwork CRs and remove ports, router interfaces and network itself"
-kuryrnetworks=`oc get kuryrnetwork -A --no-headers -o custom-columns=":status.netId,:status.subnetId,:status.routerId"`
+mapfile -t kuryrnetworks < <(oc get kuryrnetwork -A --template='{{range $i,$p := .items}}{{ $p.status.netId }}|{{ $p.status.subnetId }}|{{ $p.status.routerId}}{{"\n"}}{{end}}')
 i=0
-len=`wc -l <<< $kuryrnetworks`
-IFS=$'\n'
-for kn in $kuryrnetworks; do
-    echo "    Processing network $kn, ${i}/${len}"
-    netID=`echo $kn | awk '{print $1}'`
-    subnetID=`echo $kn | awk '{print $2}'`
-    routerID=`echo $kn | awk '{print $3}'`
+len=${#kuryrnetworks[@]}
+for kn in "${kuryrnetworks[@]}"; do
+    i=$((i+1))
+    IFS="|" read -a _a <<< $kn
+    netID=${_a[0]}
+    subnetID=${_a[1]}
+    routerID=${_a[2]}
+    echo "    Processing network $netID, ${i}/${len}"
 
     # Remove all ports from the network.
-    for port in `openstack port list --network $netID -f value -c ID`; do
+    for port in $(openstack port list --network $netID -f value -c ID); do
         ( openstack port delete $port ) &
 
         # Only allow 20 jobs in parallel.
@@ -94,6 +85,10 @@ for kn in $kuryrnetworks; do
     openstack network delete $netID
 done
 unset IFS
+
+echo "Removing Kuryr service network"
+openstack router remove subnet $routerID ${clusterID}-kuryr-service-network
+openstack network delete ${clusterID}-kuryr-service-network
 
 # All ports are gone, we can remove the security group.
 openstack security group delete kuryr-pods-security-group-${clusterID}
@@ -112,8 +107,18 @@ for existingNet in `openstack network list --tags $clusterTag -f value -c ID`; d
     fi
 done
 
+echo "Removing Kuryr finalizers from all Network Policies"
+removeNamespacedFinalizers networkpolicy kuryr.openstack.org/networkpolicy-finalizer
+
+echo "Removing Kuryr network policies"
+#for sgid in $(openstack security group list -f value -c ID -c Description | grep 'Kuryr-Kubernetes Network Policy' | cut -f 1 -d ' '); do
+#    openstack security group rule delete $(openstack security group rule list $sgid -f value -c ID | xargs)
+#    openstack security group delete $sgid
+#done
+removeNamespacedFinalizers kuryrnetworkpolicies.openstack.org kuryr.openstack.org/networkpolicy-finalizer
+
 # Remove finalizers from KuryrNetwork CRs.
-removeNamespacedFinalizers kuryrnetworks.openstack.org
+removeNamespacedFinalizers kuryrnetworks.openstack.org kuryrnetwork.finalizers.kuryr.openstack.org
 
 # FIXME(dulek): This has to be documented and decision to delete it has to be made by user. We have no way
 #               to figure out if it CNO who created it.
